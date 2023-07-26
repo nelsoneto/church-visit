5ee5178813c73665e06b29070546247b0b86bb12
nelsinho <nelsonsilvaneto1988@gmail.com>
2023-07-25 22:24:44 -0400

    ajustes formulario cadastro

--- src/pages/cadastro.js
+++ src/pages/cadastro.js
@@ -13,21 +13,24 @@ const cadastro = () => {
     </Head>
     <main className="flex items-center text-dark w-full min-h-screen dark:text-light">
      <Layout>
+        <div className='container flex items-center justify-center text-center mb-10 font-medium text-2xl con'>
+            <p>Insira as informações do visitante</p>
+        </div>
         <div className='flex justify-center items-center'>
          <form>
             <label className="block">
-                <span>Nome:</span>
-                <input type="text" className="className='mt-4 block w-96 lg:w-80 md:w-60 xs:w-32 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400'   " />
+                <span>Nome completo:</span>
+                <input type="text" className="font-medium block mb-2 w-96 lg:w-80 md:w-60 xs:w-32 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400" />
             </label>
             <label className="block">
-                <span>Sobrenome:</span>
-                <input type="text" className="className='mt-4 block w-96 lg:w-80 md:w-60 xs:w-32 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400'   " />
+                <span>Cidade:</span>
+                <input type="text" className="font-medium block mb-2 w-96 lg:w-80 md:w-60 xs:w-32 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 " />
             </label>
             <label className="block">
                 <span>Telefone:</span>
-                <input type="number" className="className='mt-4 mb-6 block w-96 lg:w-80 md:w-60 xs:w-32 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400'   " />
+                <input type="number" className="font-medium block mb-2 w-96 lg:w-80 md:w-60 xs:w-32 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400" />
             </label>
-                <input type="submit" className="className='mt-8 block w-96 lg:w-80 md:w-60 xs:w-32 px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400'   " />
+                <input type="submit" className={`font-medium block mt-8 w-96 lg:w-80 md:w-60 xs:w-32 px-3 py-2  bg-lime-900 hover:bg-lime-700 cursor-pointer text-light rounded-md shadow-lg`}  />
          </form>
         </div>
      </Layout>
